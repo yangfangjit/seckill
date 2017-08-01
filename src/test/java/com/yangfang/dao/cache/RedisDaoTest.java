4567fcf1daecfea26ebef34f6da220dbd736acdc
francisyang <francisyang@192.168.1.108>
2017-08-01 16:37:22 +0800

    replaced system.out.println with logger

--- src/test/java/com/yangfang/dao/cache/RedisDaoTest.java
+++ src/test/java/com/yangfang/dao/cache/RedisDaoTest.java
@@ -2,6 +2,8 @@ package com.yangfang.dao.cache;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -12,6 +14,8 @@ import com.yangfang.entity.Seckill;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml" })
 public class RedisDaoTest {
+	
+	private static final Logger logger = LoggerFactory.getLogger(RedisDaoTest.class);
 	
 	private long id = 1001; 
 	
@@ -27,12 +31,12 @@ public class RedisDaoTest {
 		if (seckill == null) {
 			seckill = seckillDao.queryById(id);
 			if (seckill != null) {
-				System.out.println(seckill);
+				logger.info(seckill.toString());
 				redisDao.putSeckill(seckill);
 			}
 			
 			Seckill seckill2 = redisDao.getSeckill(id);
-			System.out.println(seckill2);
+			logger.info(seckill2.toString());
 		}
 		
 	}
